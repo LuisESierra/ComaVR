93a7d376f8bf9859597a36fb686a73f989ecab72
ALEJANDRO GOMEZ LLANOS <alejandro.gomez_lla@uao.edu.co>
2022-10-29 11:50:57 -0500

    movimiento jugador

---
+++ Assets/Scripts/movimiento.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movimiento : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 direccion = new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z);
+
+        if (Input.GetKey(KeyCode.Q)) { transform.Translate(direccion * Time.deltaTime * 5, Space.World); }
+
+		if (Input.GetKey(KeyCode.S)) { transform.Translate(direccion * -1  * Time.deltaTime * 5, Space.World); }
+
+		//transform.Translate(Vector3.forward * 20, Space.Self);
+
+
+	}
+}
