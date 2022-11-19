070fec6099ee0219770c04620cd2cb7bf9d4811a
LuisESierra <luis_enrique.sierra@uao.edu.co>
2022-11-19 14:19:45 -0500

    fixing mistakes

---
+++ Assets/Scripts/movimiento.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movimiento : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 direccion = new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z);
+
+        if (Input.GetKey(KeyCode.Mouse1)) 
+		{ 
+			transform.Translate(direccion * Time.deltaTime * 5, Space.World);
+			
+			
+		}
+
+		if (Input.GetKey(KeyCode.Mouse0)) 
+		{ 
+			transform.Translate(direccion * -1  * Time.deltaTime * 5, Space.World);
+			
+		}
+
+		//transform.Translate(Vector3.forward * 20, Space.Self);
+
+
+	}
+}
